f7d86a338849732c92e4ce8c9292aa0dca6b34f1
Egor-Asist <egor.zakiev@asist-lab.com>
2026-01-19 17:47:01 +0300

    feat: expanded the capabilities of the chart and currency filter

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'ui/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Riverpod Finance',
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/providers/selected_cryptos_provider.dart
@@ -0,0 +1,56 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'selected_cryptos_provider.g.dart';
+
+@riverpod
+class SelectedCrypto extends _$SelectedCrypto {
+  static const availableCryptos = [
+    'BTC',
+    'ETH',
+    'XRP',
+    'SOL',
+    'ADA',
+    'DOGE',
+    'MATIC',
+    'LINK',
+  ];
+
+  static const cryptoColors = {
+    'BTC': 0xFFFF9500,
+    'ETH': 0xFF627EEA,
+    'XRP': 0xFF23292F,
+    'SOL': 0xFF14F195,
+    'ADA': 0xFF0033AD,
+    'DOGE': 0xFFD3A625,
+    'MATIC': 0xFF8247E5,
+    'LINK': 0xFF2A5ADA,
+  };
+
+  @override
+  String build() {
+    return 'BTC';
+  }
+
+  void setCrypto(String crypto) {
+    state = crypto;
+  }
+}
+
+@riverpod
+class FiatFilter extends _$FiatFilter {
+  @override
+  String build() {
+    return '';
+  }
+
+  void setFilter(String query) {
+    state = query.toLowerCase();
+  }
+
+  List<String> getFiltered(List<String> fiats) {
+    if (state.isEmpty) {
+      return fiats;
+    }
+    return fiats.where((fiat) => fiat.toLowerCase().startsWith(state)).toList();
+  }
+}
